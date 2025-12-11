c4eb7a8f0448f944151ebf43898419c125211ddf
Bosaif <bosaif39tm@gmail.com>
2025-12-11 21:59:31 +0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import EsignatureApp from "./Projects/01-Esignature/EsignatureApp";
+function App() {
+  return (
+    <div className="App">
+      <EsignatureApp />  {/* Use self-closing tag and correct name */}
+    </div>
+  );
+}
+
+export default App;
